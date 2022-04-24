c2a43ce3384912c00ca645b7da37ababbb743095
wlicquorice <wlicquorice@gmail.com>
2022-04-24 12:47:25 +1000

    24/04/2022 12:45PM Send/Receive requests for data from database specifically for the classes for the teacher/user logged in currently fixed bug where the Promise tag would restrict access to queries in the API back end to do this just ran it through a .then function and called an external variable through that which changed the Promise read into an INT variable

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,4 @@
 import db from "../database.js";
-import express from "express";
-import cookieParser from "cookie-parser";
 
 
 export const loginCheck = async (req, res) => {
@@ -27,3 +25,48 @@ export const loginCheck = async (req, res) => {
     };
 
 }
+export const sendClassData = async (req, res) => {
+    async function getUserId (username) {
+        var finalId;
+        await new Promise(function(resolve, reject) {
+            db.query(
+                "SELECT userId, username FROM userslogintable WHERE username = ?", [username], (error, rows, fields) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(rows[0]["userId"]);
+                        finalId = rows[0]["userId"];
+                    }
+                });
+        });
+        return finalId;
+    }
+
+    try {
+        const {username} = req.body;
+        console.log(username);
+        var userIdReceiver = '';
+        const userId = await(getUserId(username).then(function(rows) {
+            userIdReceiver = rows;
+        }));
+        db.query(
+            "SELECT c.classId, c.teacherId FROM classtable AS c, userslogintable WHERE c.teacherId = ?",
+            [userIdReceiver],
+            function (error, results, fields) {
+                try {
+                    console.log(userIdReceiver);
+
+                    res.json(results);
+                    //console.log(results);
+                } catch (err) {
+                    res.send("<h1>Error</h1>");
+                }
+
+            });
+    } catch (err) {
+        res.send("<h1>Error</h1>");
+    };
+
+}
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import {
-  loginCheck
+  loginCheck,
+    sendClassData
 } from "../controllers/controller.js";
 const router = express.Router();
 const app = express();
 router.post('/loginCheck', loginCheck);
+router.post('/sendClassData', sendClassData);
 router.get('/', (req, res) => {
   res.send('<h1>Please login</h1>');
 
